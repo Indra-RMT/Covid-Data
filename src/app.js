db2a90bcd3427211312c8b4d80dd2cda68ebab58
indra <indra.anto42@gmail.com>
2020-05-31 19:32:48 +0700

    remove file1.txt

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import "regenerator-runtime";
-import "./styles/style.css";
-import "./script/component/app-bar.js";
-import Chart from 'chart.js';
-import $ from "jquery";
-import main from "./script/view/main.js";
-
-
-document.addEventListener("DOMContentLoaded", main);
